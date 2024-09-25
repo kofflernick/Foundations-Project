75627a226526dc60a55332ec174ded16fad5de25
Nick <93751758+kofflernick@users.noreply.github.com>
2024-09-24 23:59:58 -0400

    moved register and login back into the index along with jwt stuff

--- src/controllers/employeeRouter.js
+++ src/controllers/employeeRouter.js
@@ -5,87 +5,88 @@ const jwt = require("jsonwebtoken")
 
 const employeeService = require("../services/employeeService")
 
-const secretKey = "your-secret-key"
+// const secretKey = "your-secret-key"
 
-employeeRouter.post("/register", async (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+// employeeRouter.post("/register", async (req, res) => {
+//   const username = req.body.username
+//   const password = req.body.password
 
-  try {
-    const newUser = await employeeService.addEmployee(username, password)
+//   try {
+//     const newUser = await employeeService.addEmployee(username, password)
 
-    if (!newUser) {
-      res.status(400).json({ message: "Username already taken" })
-    } else {
-      res.status(201).json({ message: "User successfully registered", newUser })
-    }
-  } catch (error) {
-    res.status(500).json({ message: "User registration unsuccessful", error })
-  }
-})
+//     if (!newUser) {
+//       res.status(400).json({ message: "Username already taken" })
+//     } else {
+//       res.status(201).json({ message: "User successfully registered", newUser })
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: "User registration unsuccessful", error })
+//   }
+// })
 
-employeeRouter.post("/login", async (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+// //problem trying to login in with a non existent user name from login[0].password
+// employeeRouter.post("/login", async (req, res) => {
+//   const username = req.body.username
+//   const password = req.body.password
 
-  const login = await employeeService.findUser(username)
-  if (!login || password !== login[0].password) {
-    console.log("here is the login password: ", login[0].password)
-    console.log("here is the req password: ", password)
-    res.status(401).json({ message: "invalid creds" })
-  } else {
-    const token = jwt.sign(
-      {
-        id: login[0].employeeID,
-        username: login[0].username,
-        status: login[0].status,
-      },
-      secretKey,
-      {
-        expiresIn: "1m",
-      }
-    )
-    res.json({ token })
-  }
-})
+//   const login = await employeeService.findUser(username)
+//   if (!login || password !== login[0].password) {
+//     console.log("here is the login password: ", login[0].password)
+//     console.log("here is the req password: ", password)
+//     res.status(401).json({ message: "invalid creds" })
+//   } else {
+//     const token = jwt.sign(
+//       {
+//         id: login[0].employeeID,
+//         username: login[0].username,
+//         status: login[0].status,
+//       },
+//       secretKey,
+//       {
+//         expiresIn: "2m",
+//       }
+//     )
+//     res.json({ token })
+//   }
+// })
 
-employeeRouter.get("/protected", authenticationToken, (req, res) => {
-  res.json({ message: "protected route accessed", user: req.user })
-})
+// employeeRouter.get("/protected", authenticationToken, (req, res) => {
+//   res.json({ message: "protected route accessed", user: req.user })
+// })
 
-async function authenticationToken(req, res, next) {
-  const authHeader = req.headers["authorization"]
-  const token = authHeader && authHeader.split(" ")[1]
+// async function authenticationToken(req, res, next) {
+//   const authHeader = req.headers["authorization"]
+//   const token = authHeader && authHeader.split(" ")[1]
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized access" })
-  }
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized access" })
+//   }
 
-  const user = await decodeJWT(token)
+//   const user = await decodeJWT(token)
 
-  if (!user) {
-    return res
-      .status(403)
-      .json({ message: "Forbidden: Invalid or expired token" })
-  }
+//   if (!user) {
+//     return res
+//       .status(403)
+//       .json({ message: "Forbidden: Invalid or expired token" })
+//   }
 
-  req.user = user
-  next()
-}
+//   req.user = user
+//   next()
+// }
 
-async function decodeJWT(token) {
-  try {
-    const user = await jwt.verify(token, secretKey)
-    return user
-  } catch (err) {
-    if (err.name === "TokenExpiredError") {
-      console.error("JWT expired:", err.message)
-      return null
-    } else {
-      console.error("JWT verification failed:", err.message)
-      return null
-    }
-  }
-}
+// async function decodeJWT(token) {
+//   try {
+//     const user = await jwt.verify(token, secretKey)
+//     return user
+//   } catch (err) {
+//     if (err.name === "TokenExpiredError") {
+//       console.error("JWT expired:", err.message)
+//       return null
+//     } else {
+//       console.error("JWT verification failed:", err.message)
+//       return null
+//     }
+//   }
+// }
 
 module.exports = employeeRouter
--- src/index.js
+++ src/index.js
@@ -19,6 +19,90 @@ app.use((req, res, next) => {
 app.use("/tickets", ticketRouter)
 app.use("/employee", employeeRouter)
 
+const secretKey = "your-secret-key"
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    const newUser = await employeeService.addEmployee(username, password)
+
+    if (!newUser) {
+      res.status(400).json({ message: "Username already taken" })
+    } else {
+      res.status(201).json({ message: "User successfully registered", newUser })
+    }
+  } catch (error) {
+    res.status(500).json({ message: "User registration unsuccessful", error })
+  }
+})
+
+//problem trying to login in with a non existent user name from login[0].password
+app.post("/login", async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const login = await employeeService.findUser(username)
+  if (!login || password !== login[0].password) {
+    console.log("here is the login password: ", login[0].password)
+    console.log("here is the req password: ", password)
+    res.status(401).json({ message: "invalid creds" })
+  } else {
+    const token = jwt.sign(
+      {
+        id: login[0].employeeID,
+        username: login[0].username,
+        status: login[0].status,
+      },
+      secretKey,
+      {
+        expiresIn: "2m",
+      }
+    )
+    res.json({ token })
+  }
+})
+
+app.get("/protected", authenticationToken, (req, res) => {
+  res.json({ message: "protected route accessed", user: req.user })
+})
+
+async function authenticationToken(req, res, next) {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized access" })
+  }
+
+  const user = await decodeJWT(token)
+
+  if (!user) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden: Invalid or expired token" })
+  }
+
+  req.user = user
+  next()
+}
+
+async function decodeJWT(token) {
+  try {
+    const user = await jwt.verify(token, secretKey)
+    return user
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      console.error("JWT expired:", err.message)
+      return null
+    } else {
+      console.error("JWT verification failed:", err.message)
+      return null
+    }
+  }
+}
+
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`)
 })
